4df5b22205cad2082de829fe5ad29cb90a43e3a0
Aaron Franke <arnfranke@yahoo.com>
2023-05-01 01:54:13 -0500

    Add OMI_seat and OMI_spawn_point extensions

---
+++ addons/omi_extensions/omi_extensions_plugin.gd
@@ -0,0 +1,11 @@
+@tool
+extends EditorPlugin
+
+
+func _enter_tree() -> void:
+	# NOTE: Be sure to also instance and register these at runtime if you want
+	# the extensions at runtime. This editor plugin script won't run in games.
+	var ext = GLTFDocumentExtensionOMISeat.new()
+	GLTFDocument.register_gltf_document_extension(ext)
+	ext = GLTFDocumentExtensionOMISpawnPoint.new()
+	GLTFDocument.register_gltf_document_extension(ext)
