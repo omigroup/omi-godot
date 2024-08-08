5343947a3ca5c0df91de7c81bdf579ff4fb2d145
Aaron Franke <arnfranke@yahoo.com>
2024-08-07 21:31:59 -0700

    Improve OMI_seat, add new node type for Area3D-derived seats

--- addons/omi_extensions/omi_extensions_plugin.gd
+++ addons/omi_extensions/omi_extensions_plugin.gd
@@ -2,12 +2,21 @@
 extends EditorPlugin
 
 
+var seat_gizmo_plugin = EditorSeat3DGizmoPlugin.new()
+
+
 func _enter_tree() -> void:
 	# NOTE: Be sure to also instance and register these at runtime if you want
 	# the extensions at runtime. This editor plugin script won't run in games.
-	var ext = GLTFDocumentExtensionOMISeat.new()
-	GLTFDocument.register_gltf_document_extension(ext)
+	var ext: GLTFDocumentExtension
+	ext = GLTFDocumentExtensionOMISeat.new()
+	GLTFDocument.register_gltf_document_extension(ext, true)
 	ext = GLTFDocumentExtensionOMISpawnPoint.new()
 	GLTFDocument.register_gltf_document_extension(ext)
 	ext = GLTFDocumentExtensionOMIPhysicsJoint.new()
 	GLTFDocument.register_gltf_document_extension(ext)
+	add_node_3d_gizmo_plugin(seat_gizmo_plugin)
+
+
+func _exit_tree():
+	remove_node_3d_gizmo_plugin(seat_gizmo_plugin)
